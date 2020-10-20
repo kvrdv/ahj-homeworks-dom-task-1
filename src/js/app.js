fce885a9d4da5bf889fe56819b1ef37f689bbde1
Semyon Kovardaev <s.kovardaev@gmail.com>
2020-10-20 10:18:39 +0300

    init commit

---
+++ src/js/app.js
@@ -0,0 +1,20 @@
+(() => {
+  const playing = true;
+  let activeHole = 1;
+
+  // const stop = () => playing = true;
+  const getHole = (index) => document.getElementById(`hole${index}`);
+  const deactivateHole = (index) => getHole(index).className = 'hole';
+  const activateHole = (index) => getHole(index).className = 'hole hole_has-mole';
+  const next = () => setTimeout(() => {
+    if (!playing) {
+      return;
+    }
+    deactivateHole(activeHole);
+    activeHole = Math.floor(1 + Math.random() * 16);
+    activateHole(activeHole);
+    next();
+  }, 800);
+
+  next();
+})();
